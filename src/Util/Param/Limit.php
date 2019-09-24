15eb84e6101e4da4ed1572be6a7e2b2ccb21d128
Dmitry <d.i.zykov@gmail.com>
2019-09-24 14:44:27 +0700

    Add validate limit

--- src/Util/Param/Limit.php
+++ src/Util/Param/Limit.php
@@ -4,6 +4,9 @@ namespace MoySklad\Util\Param;
 
 class Limit extends Param
 {
+    private const MIN_LIMIT = 1;
+    private const MAX_LIMIT = 1000;
+
     /**
      * @var int
      */
@@ -15,6 +18,10 @@ class Limit extends Param
      */
     private function __construct(int $limit)
     {
+        if ($limit < self::MIN_LIMIT || $limit > self::MAX_LIMIT) {
+            throw new \InvalidArgumentException('Allowed values range 1 - 1000');
+        }
+
         $this->limit = $limit;
         $this->type = self::LIMIT_PARAM;
     }
