9c2301e2548f5dfd29f230ef9d893721efa0b0c4
Dmitry <d.i.zykov@gmail.com>
2019-10-10 17:04:06 +0700

    Fill out custom entity client

--- src/Client/CustomEntityClient.php
+++ src/Client/CustomEntityClient.php
@@ -3,10 +3,23 @@
 namespace MoySklad\Client;
 
 use MoySklad\ApiClient;
+use MoySklad\Client\Endpoint\DeleteEntityEndpoint;
+use MoySklad\Client\Endpoint\GetEntitiesListEndpoint;
+use MoySklad\Client\Endpoint\PostEntityEndpoint;
+use MoySklad\Client\Endpoint\PutEntityEndpoint;
 use MoySklad\Entity\CustomEntity;
+use MoySklad\Entity\ListEntity;
+use MoySklad\Http\RequestExecutor;
+use MoySklad\Util\Exception\ApiClientException;
+use MoySklad\Util\Param\Param;
 
 class CustomEntityClient extends EntityClientBase
 {
+    use
+        PostEntityEndpoint,
+        DeleteEntityEndpoint,
+        PutEntityEndpoint;
+
     /**
      * CustomEntityClient constructor.
      * @param ApiClient $api
@@ -14,6 +27,77 @@ class CustomEntityClient extends EntityClientBase
     public function __construct(ApiClient $api)
     {
         parent::__construct($api, '/entity/customentity/');
+    }
+
+    /**
+     * @param string $customEntityId
+     * @param Param[] $params
+     * @return ListEntity
+     * @throws ApiClientException
+     */
+    public function getSubCustomEntitiesList(string $customEntityId, array $params = []): ListEntity
+    {
+        /** @var $listEntity ListEntity */
+        $listEntity = RequestExecutor::path($this->getApi(), $this->getPath().$customEntityId)->params($params)->get(ListEntity::class);
+
+        return $listEntity;
+    }
+
+    /**
+     * @param string $customEntityId
+     * @param CustomEntity $subCustomEntity
+     * @return CustomEntity
+     * @throws ApiClientException
+     * @throws \Exception
+     */
+    public function addSubCustomEntity(string $customEntityId, CustomEntity $subCustomEntity): CustomEntity
+    {
+        /** @var CustomEntity $subCustomEntity */
+        $subCustomEntity = RequestExecutor::path($this->getApi(), $this->getPath().$customEntityId)->body($subCustomEntity)->post(CustomEntity::class);
+
+        return $subCustomEntity;
+    }
+
+    /**
+     * @param string $customEntityId
+     * @param string $subCustomEntityId
+     * @throws ApiClientException
+     * @throws \Exception
+     */
+    public function deleteSubCustomEntity(string $customEntityId, string $subCustomEntityId): void
+    {
+        RequestExecutor::path($this->getApi(), $this->getPath().$customEntityId.'/'.$subCustomEntityId)->delete();
+    }
+
+    /**
+     * @param string $customEntityId
+     * @param string $subCustomEntityId
+     * @return CustomEntity
+     * @throws ApiClientException
+     * @throws \Exception
+     */
+    public function getSubCustomEntity(string $customEntityId, string $subCustomEntityId): CustomEntity
+    {
+        /** @var CustomEntity $subCustomEntity */
+        $subCustomEntity = RequestExecutor::path($this->getApi(), $this->getPath().$customEntityId.'/'.$subCustomEntityId)->get(CustomEntity::class);
+
+        return $subCustomEntity;
+    }
+
+    /**
+     * @param string $customEntityId
+     * @param string $subCustomEntityId
+     * @param CustomEntity $subCustomEntity
+     * @return CustomEntity
+     * @throws ApiClientException
+     * @throws \Exception
+     */
+    public function updateSubCustomEntity(string $customEntityId, string $subCustomEntityId, CustomEntity $subCustomEntity): CustomEntity
+    {
+        /** @var CustomEntity $subCustomEntity */
+        $subCustomEntity = RequestExecutor::path($this->getApi(), $this->getPath().$customEntityId.'/'.$subCustomEntityId)->body($subCustomEntity)->put(CustomEntity::class);
+
+        return $subCustomEntity;
     }
 
     /**
