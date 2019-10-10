1e7bc6cb1fcddfab321365aa2288ba642ad49b6c
Dmitry <d.i.zykov@gmail.com>
2019-10-10 17:25:32 +0700

    Fill out retail store for cashiers

--- src/Client/RetailStoreClient.php
+++ src/Client/RetailStoreClient.php
@@ -10,7 +10,12 @@ use MoySklad\Client\Endpoint\GetEntityEndpoint;
 use MoySklad\Client\Endpoint\PostEntitiesEndpoint;
 use MoySklad\Client\Endpoint\PostEntityEndpoint;
 use MoySklad\Client\Endpoint\PutEntityEndpoint;
+use MoySklad\Entity\Cashier;
+use MoySklad\Entity\ListEntity;
 use MoySklad\Entity\Store\RetailStore;
+use MoySklad\Http\RequestExecutor;
+use MoySklad\Util\Exception\ApiClientException;
+use MoySklad\Util\Param\Param;
 
 class RetailStoreClient extends EntityClientBase
 {
@@ -30,6 +35,34 @@ class RetailStoreClient extends EntityClientBase
     public function __construct(ApiClient $api)
     {
         parent::__construct($api, '/entity/retailstore/');
+    }
+
+    /**
+     * @param string $retailStoreId
+     * @param Param[] $params
+     * @return ListEntity
+     * @throws ApiClientException
+     */
+    public function getCashiersList(string $retailStoreId, array $params = []): ListEntity
+    {
+        /** @var $listEntity ListEntity */
+        $listEntity = RequestExecutor::path($this->getApi(), $this->getPath().$retailStoreId.'/cashiers')->params($params)->get(ListEntity::class);
+
+        return $listEntity;
+    }
+
+    /**
+     * @param string $retailStoreId
+     * @param string $cashierId
+     * @return Cashier
+     * @throws ApiClientException
+     */
+    public function getCashier(string $retailStoreId, string $cashierId): Cashier
+    {
+        /** @var $cashier Cashier */
+        $cashier = RequestExecutor::path($this->getApi(), $this->getPath().$retailStoreId.'/cashiers/'.$cashierId)->get(Cashier::class);
+
+        return $cashier;
     }
 
     /**
