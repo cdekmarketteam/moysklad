2ab95913f64a9540ff6e5f407089f09c719bccb4
Dmitry <d.i.zykov@gmail.com>
2019-10-10 16:35:03 +0700

    Fill out contract client

--- src/Client/ContractClient.php
+++ src/Client/ContractClient.php
@@ -3,12 +3,30 @@
 namespace MoySklad\Client;
 
 use MoySklad\ApiClient;
+use MoySklad\Client\Endpoint\DeleteEntitiesEndpoint;
+use MoySklad\Client\Endpoint\DeleteEntityEndpoint;
 use MoySklad\Client\Endpoint\GetEntitiesListEndpoint;
+use MoySklad\Client\Endpoint\GetEntityEndpoint;
+use MoySklad\Client\Endpoint\GetMetadataAttributeEndpoint;
+use MoySklad\Client\Endpoint\PostEntitiesEndpoint;
+use MoySklad\Client\Endpoint\PostEntityEndpoint;
+use MoySklad\Client\Endpoint\PutEntityEndpoint;
 use MoySklad\Entity\Contract;
+use MoySklad\Entity\Metadata\AdditionMetadata;
+use MoySklad\Http\RequestExecutor;
+use MoySklad\Util\Exception\ApiClientException;
 
 class ContractClient extends EntityClientBase
 {
-    use GetEntitiesListEndpoint;
+    use
+        GetEntitiesListEndpoint,
+        PostEntityEndpoint,
+        PostEntitiesEndpoint,
+        DeleteEntityEndpoint,
+        DeleteEntitiesEndpoint,
+        GetMetadataAttributeEndpoint,
+        GetEntityEndpoint,
+        PutEntityEndpoint;
 
     /**
      * ContractClient constructor.
@@ -17,6 +35,18 @@ class ContractClient extends EntityClientBase
     public function __construct(ApiClient $api)
     {
         parent::__construct($api, '/entity/contract/');
+    }
+
+    /**
+     * @return AdditionMetadata
+     * @throws ApiClientException
+     */
+    public function getMetadata(): AdditionMetadata
+    {
+        /** @var $additionMetadata AdditionMetadata */
+        $additionMetadata = RequestExecutor::path($this->getApi(), $this->getPath().'metadata')->get(AdditionMetadata::class);
+
+        return $additionMetadata;
     }
 
     /**
