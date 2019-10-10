b3efa21f9e57595e412e0e2e1aa8460b8bb90c83
Dmitry <d.i.zykov@gmail.com>
2019-10-10 18:12:02 +0700

    Add template endpoint to assortment

--- src/Client/AssortmentClient.php
+++ src/Client/AssortmentClient.php
@@ -6,6 +6,11 @@ use MoySklad\ApiClient;
 use MoySklad\Client\Endpoint\DeleteEntitiesEndpoint;
 use MoySklad\Client\Endpoint\GetEntitiesListEndpoint;
 use MoySklad\Entity\Assortment;
+use MoySklad\Entity\ListEntity;
+use MoySklad\Entity\Template\EmbeddedTemplate;
+use MoySklad\Http\RequestExecutor;
+use MoySklad\Util\Exception\ApiClientException;
+use MoySklad\Util\Param\Param;
 
 class AssortmentClient extends EntityClientBase
 {
@@ -20,6 +25,32 @@ class AssortmentClient extends EntityClientBase
     public function __construct(ApiClient $api)
     {
         parent::__construct($api, '/entity/assortment/');
+    }
+
+    /**
+     * @param Param[] $params
+     * @return ListEntity
+     * @throws ApiClientException
+     */
+    public function getEmbeddedTemplatesList(array $params = []): ListEntity
+    {
+        /** @var $listEntity ListEntity */
+        $listEntity = RequestExecutor::path($this->getApi(), $this->getPath().'metadata/embeddedtemplate')->params($params)->get(ListEntity::class);
+
+        return $listEntity;
+    }
+
+    /**
+     * @param string $embeddedTemplateId
+     * @return EmbeddedTemplate
+     * @throws ApiClientException
+     */
+    public function getEmbeddedTemplate(string $embeddedTemplateId): EmbeddedTemplate
+    {
+        /** @var $embeddedTemplate EmbeddedTemplate */
+        $embeddedTemplate = RequestExecutor::path($this->getApi(), $this->getPath().'metadata/embeddedtemplate/'.$embeddedTemplateId)->get(EmbeddedTemplate::class);
+
+        return $embeddedTemplate;
     }
 
     /**
