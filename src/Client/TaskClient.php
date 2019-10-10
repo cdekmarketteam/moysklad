9c290519723c4f6ab053c18b48a1d04f46d722d8
Dmitry <d.i.zykov@gmail.com>
2019-10-10 17:48:40 +0700

    Fill out task client

--- src/Client/TaskClient.php
+++ src/Client/TaskClient.php
@@ -3,12 +3,30 @@
 namespace MoySklad\Client;
 
 use MoySklad\ApiClient;
+use MoySklad\Client\Endpoint\DeleteEntitiesEndpoint;
+use MoySklad\Client\Endpoint\DeleteEntityEndpoint;
 use MoySklad\Client\Endpoint\GetEntitiesListEndpoint;
+use MoySklad\Client\Endpoint\GetEntityEndpoint;
+use MoySklad\Client\Endpoint\PostEntitiesEndpoint;
+use MoySklad\Client\Endpoint\PostEntityEndpoint;
+use MoySklad\Client\Endpoint\PutEntityEndpoint;
+use MoySklad\Entity\ListEntity;
 use MoySklad\Entity\Task;
+use MoySklad\Entity\TaskNote;
+use MoySklad\Http\RequestExecutor;
+use MoySklad\Util\Exception\ApiClientException;
+use MoySklad\Util\Param\Param;
 
 class TaskClient extends EntityClientBase
 {
-    use GetEntitiesListEndpoint;
+    use
+        GetEntitiesListEndpoint,
+        PostEntityEndpoint,
+        PostEntitiesEndpoint,
+        DeleteEntityEndpoint,
+        DeleteEntitiesEndpoint,
+        GetEntityEndpoint,
+        PutEntityEndpoint;
 
     /**
      * TaskClient constructor.
@@ -17,6 +35,79 @@ class TaskClient extends EntityClientBase
     public function __construct(ApiClient $api)
     {
         parent::__construct($api, '/entity/task/');
+    }
+
+    /**
+     * @param string $taskId
+     * @param Param[] $params
+     * @return ListEntity
+     * @throws ApiClientException
+     */
+    public function getTaskNotesList(string $taskId, array $params = []): ListEntity
+    {
+        /** @var $listEntity ListEntity */
+        $listEntity = RequestExecutor::path($this->getApi(), $this->getPath().$taskId.'/notes')->params($params)->get(ListEntity::class);
+
+        return $listEntity;
+    }
+
+    /**
+     * @param string $taskId
+     * @param TaskNote $taskNote
+     * @return TaskNote[]
+     * @throws ApiClientException
+     * @throws \Exception
+     */
+    public function createTaskNote(string $taskId, TaskNote $taskNote): array
+    {
+        $className = TaskNote::class;
+
+        /** @var TaskNote[] $taskNotes */
+        $taskNotes = RequestExecutor::path($this->getApi(), $this->getPath().$taskId.'/notes')->body($taskNote)->post("array<{$className}>");
+
+        return $taskNotes;
+    }
+
+    /**
+     * @param string $taskId
+     * @param string $taskNoteId
+     * @return TaskNote
+     * @throws ApiClientException
+     * @throws \Exception
+     */
+    public function getTaskNote(string $taskId, string $taskNoteId): TaskNote
+    {
+        /** @var TaskNote $taskNote */
+        $taskNote = RequestExecutor::path($this->getApi(), $this->getPath().$taskId.'/notes/'.$taskNoteId)->get(TaskNote::class);
+
+        return $taskNote;
+    }
+
+    /**
+     * @param string $taskId
+     * @param string $taskNoteId
+     * @param TaskNote $updatedTaskNote
+     * @return TaskNote
+     * @throws ApiClientException
+     * @throws \Exception
+     */
+    public function updateTaskNote(string $taskId, string $taskNoteId, TaskNote $updatedTaskNote): TaskNote
+    {
+        /** @var TaskNote $taskNote */
+        $taskNote = RequestExecutor::path($this->getApi(), $this->getPath().$taskId.'/notes/'.$taskNoteId)->body($updatedTaskNote)->put(TaskNote::class);
+
+        return $taskNote;
+    }
+
+    /**
+     * @param string $taskId
+     * @param string $taskNoteId
+     * @throws ApiClientException
+     * @throws \Exception
+     */
+    public function deleteTaskNote(string $taskId, string $taskNoteId): void
+    {
+        RequestExecutor::path($this->getApi(), $this->getPath().$taskId.'/notes/'.$taskNoteId)->delete();
     }
 
     /**
