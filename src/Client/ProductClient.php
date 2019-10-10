eab14c885114a09c4890cf9b8c9470d77329d6d6
Dmitry <d.i.zykov@gmail.com>
2019-10-10 14:18:58 +0700

    Fill out product client

--- src/Client/ProductClient.php
+++ src/Client/ProductClient.php
@@ -3,7 +3,15 @@
 namespace MoySklad\Client;
 
 use MoySklad\ApiClient;
+use MoySklad\Client\Endpoint\DeleteEntitiesEndpoint;
+use MoySklad\Client\Endpoint\DeleteEntityEndpoint;
 use MoySklad\Client\Endpoint\GetEntitiesListEndpoint;
+use MoySklad\Client\Endpoint\GetEntityEndpoint;
+use MoySklad\Client\Endpoint\GetMetadataAttributeEndpoint;
+use MoySklad\Client\Endpoint\GetMetadataEndpoint;
+use MoySklad\Client\Endpoint\PostEntitiesEndpoint;
+use MoySklad\Client\Endpoint\PostEntityEndpoint;
+use MoySklad\Client\Endpoint\PutEntityEndpoint;
 use MoySklad\Entity\Image;
 use MoySklad\Entity\ListEntity;
 use MoySklad\Entity\Product\Product;
@@ -13,7 +21,16 @@ use MoySklad\Util\Param\Param;
 
 class ProductClient extends EntityClientBase
 {
-    use GetEntitiesListEndpoint;
+    use
+        GetEntitiesListEndpoint,
+        PostEntityEndpoint,
+        PostEntitiesEndpoint,
+        DeleteEntityEndpoint,
+        DeleteEntitiesEndpoint,
+        GetMetadataEndpoint,
+        GetMetadataAttributeEndpoint,
+        GetEntityEndpoint,
+        PutEntityEndpoint;
 
     /**
      * ProductClient constructor.
